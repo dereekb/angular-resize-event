ab642793288541c6093ef24f979c9815443cf565
Derek Burgman <derek@dereekb.com>
2024-01-01 15:23:42 -0600

    release: angular 16 dist build

--- public-api.d.ts
+++ public-api.d.ts
@@ -1,7 +1,3 @@
-/*
- * Public API Surface of angular-resize-event
- */
-
 export { ResizedEvent } from './lib/resized.event';
 export * from './lib/resized.directive';
 export * from './lib/angular-resize-event.module';
